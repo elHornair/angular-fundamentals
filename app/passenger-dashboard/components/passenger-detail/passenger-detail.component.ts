9e127f2ad0571d63fc6afb2ad82426b57a7537f9
Alain Horner <alain.hornair@gmail.com>
2019-09-13 15:37:23 +0200

    passenger detail component (dumb compoenent)

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
+import { PassengerDashboardModule } from '../../passenger-dashboard.module';
 
 @Component({
   selector: 'passenger-detail',
+  styleUrls: ['passenger-detail.component.scss'],
   template: `
-    <div>Detail component</div>
+    <div>
+      <span
+        class="status"
+        [ngClass]="{'checked-in': detail.checkedIn}"
+      ></span>
+      {{ detail.fullname }}
+      <div class="date">
+        Check in date:
+        {{ detail.checkInDate ? (detail.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
+      </div>
+      <div class="children">
+        Children: {{ detail.children?.length || 0 }}
+      </div>
+    </div>
   `
 })
 export class PassengerDetailComponent {
+  @Input()
+  detail: Passenger;
   constructor() {}
 }
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -10,25 +10,11 @@ import { Passenger } from '../../models/passenger.interface'
           [items]="passengers"
         >
         </passenger-count>
-        <passenger-detail>
+        <passenger-detail
+          *ngFor="let passenger of passengers;"
+          [detail]="passenger"
+        >
         </passenger-detail>
-        <ul>
-          <li *ngFor="let passenger of passengers; let i = index;">
-          <span
-            class="status"
-            [ngClass]="{'checked-in': passenger.checkedIn}"
-          ></span>
-            {{ i }}: {{ passenger.fullname }}
-            <p>{{ passenger | json }}</p>
-            <div class="date">
-              Check in date:
-              {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
-            </div>
-            <div class="children">
-              Children: {{ passenger.children?.length || 0 }}
-            </div>
-          </li>
-        </ul>
       </div>
     `
 })
